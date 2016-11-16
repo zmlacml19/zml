fc9b7ef63d39d770977653b7f6313f2babe441cd
klchejian <501857146@qq.com>
2016-11-16 11:44:15 +0800

    add variables about HOS

--- conf/env.sh
+++ conf/env.sh
@@ -2,3 +2,12 @@
 
 # the path to the tool configuration
 export SLAVES_CONF_NAME="slaves.property"
+export HOS_HOME=`pwd`
+export HIVE_DIR=$HOS_HOME/softwares/apache-hive-2.2.0-SNAPSHOT-bin
+export SPARK_DIR=$HOS_HOME/softwares/spark-2.0.0-bin-hadoop2-without-hive
+export BB_DIR=$HOS_HOME/softwares/BB_HoS
+export SPARK_HOME=$HOS_HOME/softwares/spark-2.0.0-bin-hadoop2-without-hive
+export HIVE_HOME=$HOS_HOME/softwares/apache-hive-2.2.0-SNAPSHOT-bin
+
+
+
